bf21bd940d5e8d78d155f40120dc320e608db6ec
Anitha <anitha@iweensoft.com>
2023-11-23 15:45:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./db.js");
+const userRouter = require("./routes/user.routes.js");
+const noteRouter = require("./routes/note.routes.js");
+require("dotenv").config();
+const port = process.env.PORT;
+const app = express();
+
+const corsOptions = {
+  origin: "https://notestakingwebapplication.netlify.app", // replace with your frontend's URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // enable passing of cookies from the frontend
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/note", noteRouter);
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "api is working now",
+  });
+});
+
+app.listen(port, async () => {
+  try {
+    await db;
+    console.log("Database is connected");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+  console.log("Server is running on port number", port);
+});
